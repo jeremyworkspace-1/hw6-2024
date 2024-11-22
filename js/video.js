22d0a4cf08a368f2e4cdadc58df3319a8a1a71ad
Xinjian Li <xinjian@umich.edu>
2024-11-21 20:47:44 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,46 +1,50 @@
 var video;
 
 window.addEventListener("load", function () {
-	console.log("Good job opening the window");
-	video = document.querySelector("#player1");
+	console.log("Good job opening the window")
+	video = document.querySelector("#player1")
 	video.autoplay = false;
 	video.loop = false;
+	console.log("Autoplay is " + video.autoplay)
+	console.log("Loop is " + video.loop)
 });
 
 document.querySelector("#play").addEventListener("click", function () {
-	console.log("Play Video");
-	video.play();
-	document.querySelector("#volume").innerText = `${Math.round(video.volume * 100)}%`;
+	console.log("Play Video")
+	video.play()
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
 });
 
 document.querySelector("#pause").addEventListener("click", function () {
-	console.log("Pause Video");
-	video.pause();
+	console.log("Pause Video")
+	video.pause()
 });
 
 document.querySelector("#slower").addEventListener("click", function () {
-	console.log("log the new speed");
-	video.playbackRate = 0.5;
+	console.log("slow down video")
+	video.playbackRate *= .90
+	console.log("Speed is", video.playbackRate)
 });
 
 document.querySelector("#faster").addEventListener("click", function () {
-	console.log("log the new speed");
-	video.playbackRate = 2.0;
+	console.log("faster video")
+	video.playbackRate /= .90
+	console.log("Speed is", video.playbackRate)
 });
 
 document.querySelector("#skip").addEventListener("click", function () {
-	console.log("current location of the video");
 	if (video.currentTime + 10 >= video.duration) {
 		video.currentTime = 0;
 	} else {
 		video.currentTime += 10;
 	}
+	console.log("current location of the video");
 });
 
 document.querySelector("#mute").addEventListener("click", function () {
-	video.muted = !video.muted;
-	this.innerText = video.muted ? "Unmute" : "Mute";
-	console.log("Muted: " + video.muted);
+	video.muted = !video.muted
+	this.innerText = video.muted ? "Unmute" : "Mute"
+	console.log("Muted: " + video.muted)
 });
 
 document.querySelector("#slider").addEventListener("input", function () {
