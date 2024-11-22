9c9d8fbe04adf0d7cdaaba0e5c32c8051a5ff468
Xinjian Li <xinjian@umich.edu>
2024-11-21 23:17:49 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -7,57 +7,59 @@ window.addEventListener("load", function () {
 	video.loop = false;
 	console.log("Autoplay is " + video.autoplay)
 	console.log("Loop is " + video.loop)
-});
 
-document.querySelector("#play").addEventListener("click", function () {
-	console.log("Play Video")
-	video.play()
-	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
-});
-
-document.querySelector("#pause").addEventListener("click", function () {
-	console.log("Pause Video")
-	video.pause()
-});
-
-document.querySelector("#slower").addEventListener("click", function () {
-	console.log("slow down video")
-	video.playbackRate *= .90
-	console.log("Speed is", video.playbackRate)
-});
-
-document.querySelector("#faster").addEventListener("click", function () {
-	console.log("faster video")
-	video.playbackRate /= .90
-	console.log("Speed is", video.playbackRate)
-});
-
-document.querySelector("#skip").addEventListener("click", function () {
-	if (video.currentTime + 10 >= video.duration) {
-		video.currentTime = 0;
-	} else {
-		video.currentTime += 10;
-	}
-	console.log("current location of the video");
-});
-
-document.querySelector("#mute").addEventListener("click", function () {
-	video.muted = !video.muted
-	this.innerText = video.muted ? "Unmute" : "Mute"
-	console.log("Muted: " + video.muted)
-});
-
-document.querySelector("#slider").addEventListener("input", function () {
-	video.volume = this.value / 100;
-	document.querySelector("#volume").textContent = video.volume * 100 + "%";
-});
-
-
-document.querySelector("#vintage").addEventListener("click", function() {
-    video.classList.add("oldSchool");
-});
-
-document.querySelector("#orig").addEventListener("click", function() {
-    video.classList.remove("oldSchool");
+	document.querySelector("#play").addEventListener("click", function () {
+		console.log("Play Video")
+		video.play()
+		document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
+	});
+	
+	document.querySelector("#pause").addEventListener("click", function () {
+		console.log("Pause Video")
+		video.pause()
+	});
+	
+	document.querySelector("#slower").addEventListener("click", function () {
+		console.log("slow down video")
+		video.playbackRate *= .90
+		console.log("Speed is", video.playbackRate)
+	});
+	
+	document.querySelector("#faster").addEventListener("click", function () {
+		console.log("faster video")
+		video.playbackRate /= .90
+		console.log("Speed is", video.playbackRate)
+	});
+	
+	document.querySelector("#skip").addEventListener("click", function () {
+		if (video.currentTime + 10 >= video.duration) {
+			video.currentTime = 0;
+		} else {
+			video.currentTime += 10;
+		}
+		console.log("current location of the video");
+	});
+	
+	document.querySelector("#mute").addEventListener("click", function () {
+		video.muted = !video.muted
+		this.innerText = video.muted ? "Unmute" : "Mute"
+		console.log("Muted: " + video.muted)
+	});
+	
+	document.querySelector("#slider").addEventListener("input", function () {
+		video.volume = this.value / 100;
+		document.querySelector("#volume").textContent = video.volume * 100 + "%";
+	});
+	
+	
+	document.querySelector("#vintage").addEventListener("click", function() {
+		video.classList.add("oldSchool");
+	});
+	
+	document.querySelector("#orig").addEventListener("click", function() {
+		video.classList.remove("oldSchool");
+	});
+	
+	
 });
 
